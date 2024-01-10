f3704a35260df1be9f350993b2bd77cb9179c932
James Settles <settles.james.b@gmail.com>
2024-01-10 09:44:09 -0800

    Update Mock_Query.sql

--- Mock_Query.sql
+++ Mock_Query.sql
@@ -1,4 +1,4 @@
--— Purpose mock query to show each SQL functions in relation to MSSQL Server syntax and order
+-- Purpose mock query to show each SQL functions in relation to MSSQL Server syntax and order 
 
 -- SELECT Clause Functions
 SELECT
